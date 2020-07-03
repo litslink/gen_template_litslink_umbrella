1af364a2a38ff6abd2e9821e39e771c05b68ce1a
Dmytro V <staff1489@gmail.com>
2020-07-03 16:21:55 +0300

    Use db, auth apps for api

--- template/$PROJECT_NAME$/apps/api/mix.exs
+++ template/$PROJECT_NAME$/apps/api/mix.exs
@@ -22,6 +22,8 @@ defmodule API.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:db,   in_umbrella: true},
+      {:auth, in_umbrella: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
