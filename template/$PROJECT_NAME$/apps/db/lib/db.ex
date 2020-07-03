e4315ed314121a434051d7498c766c7876d50a8b
Dmytro V <staff1489@gmail.com>
2020-07-03 16:18:11 +0300

    Mix project DB sceleton

---
+++ template/$PROJECT_NAME$/apps/db/lib/db.ex
@@ -0,0 +1,18 @@
+defmodule DB do
+  @moduledoc """
+  Documentation for `DB`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DB.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
