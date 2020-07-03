47877b9c3a730043e5cf1052a01c114b35d654b0
Dmytro V <staff1489@gmail.com>
2020-07-03 17:18:11 +0300

    phx web app with --live --no-ecto

--- template/$PROJECT_NAME$/config/config.exs
+++ template/$PROJECT_NAME$/config/config.exs
@@ -9,6 +9,17 @@
 # move said applications out of the umbrella.
 import Config
 
+config :web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :web, Web.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "02XoOcPpbI+8qzLb5Dg9P0ZuiHBSRWvN0AjeexpNdtE/GFrlrjvU6QLg/EtvfUv2",
+  render_errors: [view: Web.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Web.PubSub,
+  live_view: [signing_salt: "hoVhGHRi"]
+
 # Sample configuration:
 #
 #     config :logger, :console,
@@ -16,3 +27,15 @@ import Config
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
